4331e731b2029d662ed352e13db2d3634be31a32
Petru Isfan <petru.isfan+git@gmail.com>
2013-06-20 13:43:47 +0300

    test

--- server/server.js
+++ server/server.js
@@ -1,85 +1,128 @@
-#!/usr/bin/nodejs
+#!/usr/local/bin/node
 
 var express = require('express');
 var fs = require('fs');
-var sys = require('util')
-var exec = require('child_process').exec;
-var sleep = require("sleep");
+var spawn = require('child_process').spawn;
 var app = express();
 var child = null;
 
-var serverRoot="/home/petru/data/music/petre/";
+var serverRoot="/data/media/music/";
 var player="mplayer";
 
 
+/**
+ * Return the html files.
+ */
+app.use(express.static(".."));
+
 app.get('/webplayer/rest/list/*', function(req, res) {
-    var id = req.params;
+    var id = req.params,
+        json = [],
+        dir =  serverRoot + id;
+
     console.log("requesting " + id);
-    var json = [];
-    fs.readdir(serverRoot + id, function (err, files) {
-        if (err) {
-//            throw err;
-            res.send("Error reading files");
-        }
-        for (var index in files) {
-            json.push(files[index]);
-        }
-        res.send({"files": json});
-    });
+    //
+    // if requested file is directory
+    // 
+    if (fs.lstatSync(dir).isDirectory()) {
+        fs.readdir(serverRoot + id, function (err, files) {
+            if (err) {
+                res.send("Error reading files");
+            }
+            for (var index in files) {
+                json.push(files[index]);
+            }
+            res.send({"files": json});
+        });
+    } else {
+        res.send("Requested file is not a directory!");
+    }
 });
 
 app.get('/webplayer/rest/play/*', function(req, res) {
-    if ( child != null) {
-        stopPlayback();
-    }
-
-    var id = req.params;
-    console.log("playing " + id);
-    var cmd = player + " '" + serverRoot + id + "'"
-    console.log("-------------------------------------");
-    console.log(cmd)
-    console.log("-------------------------------------");
-    child = exec(cmd, puts);
-    res.send("OK");
-});
-
-app.get('/webplayer/rest/playAll/*', function(req, res) {
-    if ( child != null) {
-        stopPlayback();
-    }
-
-    var id = req.params;
-    // 
-    // This works only if the folder does not contain spaces or special characters!
     //
-    var cmd = player + " " + serverRoot + id + "/*"
-    console.log("-------------------------------------");
-    console.log(cmd)
-    console.log("-------------------------------------");
-    child = exec(cmd, puts);
+    // First thing first! Before playing, stop other playing songs.
+    //
+    stopPlayback();
+
+    var id = req.params,
+        file = serverRoot + id;
+
+    console.log("Playing " + file);
+
+    child = spawn(player, ["-slave", file]);
+    child.on('close', function (code) {
+        if (code !== 0) {
+            console.log('Mplayer process exited with code ' + code);
+        }
+    });
+
+    res.send("Playing");
+});
+
+/**
+ * Play all files in folder
+ * TODO: not yet functional
+ */
+app.get('/webplayer/rest/playall/*', function(req, res) {
+    //
+    // First thing first! Before playing, stop other playing songs.
+    //
+    stopPlayback();
+
+    var id = req.params,
+        dir = serverRoot + id;
+
+    console.log("Playing all files in: " + dir);
+
+    if (fs.lstatSync(dir).isDirectory()) {
+        child = spawn(player, ["-slave", dir + "*"]);
+        child.on('close', function (code) {
+            if (code !== 0) {
+                console.log('Mplayer process exited with code ' + code);
+            }
+        });
+        
+        res.send("Playing");
+    } else {
+        res.send("The file specified is not a folder!");
+    }
+});
+
+/**
+ * Stop the curent child process (playback).
+ */
+app.get('/webplayer/rest/stop', function(req, res) {
+    stopPlayback();
     res.send("OK");
 });
 
-app.get('/webplayer/rest/stop', function(req, res) {
-    if (child != null) {
-        stopPlayback();
+/**
+ * Pause the current child process (playback).
+ */
+app.get('/webplayer/rest/pause', function(req, res) {
+    if (child) {
+        child.stdin.write("p\n")
+    }
+    res.send("OK");
+});
+
+/**
+ * Play next file in the current child process.
+ */
+app.get('/webplayer/rest/next', function(req, res) {
+    if (child) {
+        child.stdin.write(">\n")
     }
     res.send("OK");
 });
 
 var stopPlayback = function() {
-    exec("pkill " + player);
-    child = null;
-    sleep.sleep(1);
-}
-
-var puts = function(error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
+    if (child) {
+        child.kill();
     }
 }
+
 app.listen(3000);
 console.log('Node.js server listening on port 3000...');
 
